1818d545d92a1d9ca2e12512560ed44228d2e40b
Davide <bibbodavide@gmail.com>
2021-02-10 10:17:23 +0100

    aggiunta milestone-2 e risolto primo punto del voto

---
+++ milestone-2/main.js
@@ -0,0 +1,54 @@
+// definisco variabili API
+const apiUri = 'https://api.themoviedb.org/3/';
+const apiKey = 'd697a14c56f5dc39b8a8034bfcea2fc1';
+
+// parte Vue Js
+new Vue({
+  el: '#app',
+  data: {
+    results: [],
+    searchText: '',
+    titleText: '',
+    loading: false
+  },
+  methods: {
+    search() {
+
+      if(this.searchText !== '') {
+        // la proprietà loading diventa true
+        this.loading = true;
+
+        // svuoto/ripristino input di ricerca e risultati
+        this.results = [];
+        let userText = this.searchText;
+        this.searchText = '';
+
+        // e metto il testo ricercato nel titolo
+        this.titleText = userText;
+
+        // da qui , faccio partire la chiamata ajax con axios a tmdb
+        // passo l'apikey come parametro all'url tramite un oggetto usato come secondo parametro di axios
+        axios.get(apiUri + 'search/movie', {
+          params: {
+            api_key: apiKey,
+            query: userText
+          }
+        })
+          .then((resp) => {
+            console.log('Array di film selezionati: ', resp.data.results);
+            this.results = resp.data.results;
+            // la proprietà loading diventa di nuovo false
+            this.loading = false;
+          });
+
+      }
+    },
+    getStars(vote) {
+      return Math.ceil(vote / 2);
+    }
+  },
+  mounted() {
+
+  }
+});
+Vue.config.devtools = true;
