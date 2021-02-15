c9132534f473f98edd7b923514bb41a9f2c0eb6f
Davide <bibbodavide@gmail.com>
2021-02-15 15:02:09 +0100

    modifiche

--- milestone opzionali/main.js
+++ milestone opzionali/main.js
@@ -12,11 +12,11 @@ new Vue({
     searchText: '',
     titleText: '',
     loading: false,
+    pageNotFound: false,
     genresArray: [],
     credits: [],
   },
   mounted() {
-
   },
   methods: {
     search() {
@@ -44,6 +44,10 @@ new Vue({
         })
           .then((resp) => {
             console.log('Array di film selezionati: ', resp.data.results);
+            resp.data.results.forEach(function(item){
+              item.tv = false
+              item.movie = true
+            });
             // usato spread operator
             this.results = [...this.results, ...resp.data.results];
             // la proprietà loading diventa di nuovo false
@@ -60,6 +64,10 @@ new Vue({
         })
           .then((resp) => {
             console.log('Array di serie tv selezionate: ', resp.data.results);
+            resp.data.results.forEach(function(item){
+              item.tv = true
+              item.movie = false
+            });
             // usato spread operator
             this.results = [...this.results, ...resp.data.results];
             // la proprietà loading diventa di nuovo false
@@ -123,26 +131,46 @@ new Vue({
       return elementGenre;
     },
     // funzione per mostrare gli attori con una chiamata ajax con axios
-    getActors(elementId){
+    getActors(element){
       const self = this;
-      return axios.get(apiUri + 'movie/' + elementId + '/credits', {
-        params: {
-          api_key: apiKey,
-        }
-      })
-      .then(function(resp) {
-        self.credits = resp.data.cast.slice(0,5);
-        console.log('Array del cast di attori di film: ', resp.data.cast.slice(0,5));
-      });
-      return axios.get(apiUri + 'tv/' + elementId + '/credits', {
-        params: {
-          api_key: apiKey,
-        }
-      })
-      .then(function(resp) {
-        self.credits = resp.data.cast.slice(0,5);
-        console.log('Array del cast di attori di serie tv: ', resp.data.cast.slice(0,5));
-      });
+
+      const elementId = element.id
+      // prima resetto array credits
+      self.credits = [];
+      // poi faccio if and else per stabilire se è un film o una serie tv
+      if(element.movie === true) {
+        axios.get(apiUri + 'movie/' + elementId + '/credits', {
+          params: {
+            api_key: apiKey,
+          }
+        })
+        .then(function(resp) {
+          self.credits = resp.data.cast.slice(0,5);
+          console.log('movie', elementId, 'Array del cast di attori di film: ', resp.data.cast.slice(0,5));
+        });
+
+      } else {
+        axios.get(apiUri + 'tv/' + elementId + '/credits', {
+          params: {
+            api_key:  apiKey,
+          }
+        })
+        .then(function(resp) {
+          self.credits = resp.data.cast.slice(0,5);
+          console.log('tv', elementId, 'Array del cast di attori di serie tv: ', resp.data.cast.slice(0,5));
+        });
+      }
+    },
+    // funzione di errore di inserimento apikey
+    apiKeyNotValid() {
+
+      if (this.apiKey.length < 32) {
+        this.pageNotFound = true;
+        console.log('api_key wrong')
+      } else if (this.apiKey !== 'd697a14c56f5dc39b8a8034bfcea2fc1') {
+        this.pageNotFound = true;
+        console.log('api_key not valid')
+      }
     },
   },
 });
